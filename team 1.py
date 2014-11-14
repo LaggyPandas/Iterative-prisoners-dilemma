2078089e117fb6108db9d59cb2a447bd5c4a4617
cgscraps <cgscraps@gmail.com>
2014-11-14 13:55:51 -0600

    team1
    we did not change anything because i have no idea what is going on

---
+++ team 1.py
@@ -0,0 +1,9 @@
+######
+    ######
+    #
+    #This example player always betrays.      
+    elif player == 1:
+        if getting_team_name:
+            return 'perticipated appex soundsystem'
+        else:
+            return 'b'
