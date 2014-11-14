fbfeb470312e71ecd17d8b8902d6f5c03b814d00
Sarah Smith <ssflowerchildss@gmail.com>
2014-11-14 13:59:02 -0600

    fire penguin disco pandas

---
+++ fire penguin disco pandas.py
@@ -0,0 +1,12 @@
+######
+    ######        
+    #
+    elif player == 6:
+        if getting_team_name:
+            return 'loyal vengeful'
+        else:
+            size = len(histort)
+            if(size%3==0):
+                return'c'
+            else:
+                return'b'
